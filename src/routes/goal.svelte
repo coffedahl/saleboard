761aff3e3646c683391e53e6359439e39d05e313
coffedahl <coffedahl@hotmail.com>
2023-09-09 16:11:49 +0200

    Created component for goal

---
+++ src/routes/goal.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let title: string
+    export let data: any
+</script>
+
+<div>
+	<h1>{title}</h1>
+	<h2>{data}</h2>
+</div>
+
+<style lang="scss">
+    div{
+        background-color: transparent;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
